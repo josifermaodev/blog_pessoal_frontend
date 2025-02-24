c47f2c18c62559dc436d9ba487ef4cc09f946628
josifermaodev <josianefermao@gmail.com>
2025-02-24 10:43:03 -0300

    Atualização de estilização da home e da navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,51 +1,88 @@
-import { ReactNode, useContext } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../contexts/AuthContext"
+import { ReactNode, useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 import { ToastAlerta } from "../../utils/ToastAlerta";
+import { Menu, X } from "lucide-react"; // Ícones do Lucide para o menu
 
 function Navbar() {
-
     const navigate = useNavigate();
     const { usuario, handleLogout } = useContext(AuthContext);
-    
+    const [menuAberto, setMenuAberto] = useState(false);
+
     function logout() {
         handleLogout();
-        ToastAlerta('O Usuário foi desconectado com sucesso!', 'info');
+        ToastAlerta("O Usuário foi desconectado com sucesso!", "info");
         navigate("/");
     }
 
-    let component: ReactNode
+    let component: ReactNode;
 
     if (usuario.token !== "") {
-
         component = (
+            <div className="w-full flex justify-center items-center bg-gradient-to-r from-blue-500 to-green-500 text-white h-15">
+                <div className="container flex justify-between items-center p-4">
+                    {/* Logo */}
+                    <Link to="/home" className="font-bold hover:text-blue-950 font-dancing text-3xl">
+                        Blog Pessoal
+                    </Link>
 
-            <div className='w-full flex justify-center items-center bg-gradient-to-r from-blue-500 to-green-500 text-white h-15'>
+                    {/* Botão do menu-hambúrguer (somente visível em telas pequenas) */}
+                    <button 
+                        onClick={() => setMenuAberto(!menuAberto)} 
+                        className="md:hidden text-white focus:outline-none"
+                        aria-label="Abrir menu"
+                    >
+                        {menuAberto ? <X size={28} /> : <Menu size={28} />}
+                    </button>
 
-                <div className="container flex justify-between">
-                    <Link to='/home' className="hover:text-blue-950 text-lg font-bold">Blog Pessoal</Link>
-
-                    <div className='flex gap-5 items-center text-lg font-bold'>
-                        <Link to='/postagens' className='relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full pb-1 hover:text-blue-950'>Postagens</Link>
-                        <Link to='/temas' className='relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full pb-1 hover:text-blue-950'>Temas</Link>
-                        <Link to='/cadastrartema' className='relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full pb-1 hover:text-blue-950'>Cadastrar tema</Link>
-                        <Link to='/perfil' className='relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full pb-1 hover:text-blue-950'>Perfil</Link>
-                        <Link to='' onClick={logout} className='relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full pb-1 hover:text-blue-950'>Sair</Link>
+                    {/* Menu de navegação */}
+                    <div
+                        className={`absolute md:static top-16 right-0 ${
+                            menuAberto ? "bg-gradient-to-r from-blue-500 to-green-500" : "bg-blue-500 md:bg-transparent"
+                        } w-full md:w-auto flex flex-col md:flex-row md:gap-5 items-center text-lg font-bold transition-all duration-300 ${
+                            menuAberto ? "flex" : "hidden md:flex"
+                        }`}
+                    >
+                        <Link
+                            to="/postagens"
+                            className="py-2 px-4 md:p-0 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full hover:text-blue-950"
+                        >
+                            Postagens
+                        </Link>
+                        <Link
+                            to="/temas"
+                            className="py-2 px-4 md:p-0 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full hover:text-blue-950"
+                        >
+                            Temas
+                        </Link>
+                        <Link
+                            to="/cadastrartema"
+                            className="py-2 px-4 md:p-0 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full hover:text-blue-950"
+                        >
+                            Cadastrar tema
+                        </Link>
+                        <Link
+                            to="/perfil"
+                            className="py-2 px-4 md:p-0 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full hover:text-blue-950"
+                        >
+                            Perfil
+                        </Link>
+                        <Link
+                            to=""
+                            onClick={logout}
+                            className="py-2 px-4 md:p-0 relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-blue-950 after:transition-all after:duration-300 hover:after:w-full hover:text-blue-950"
+                        >
+                            Sair
+                        </Link>
                     </div>
                 </div>
             </div>
-
-        )
-
+        );
     }
 
-    return (
-        <>
-            { component }
-        </>
-    )
-    
+    return <>{component}</>;
 }
 
-export default Navbar
+export default Navbar;
+
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,29 @@
-import ListaPostagens from "../../components/postagem/listapostagem/ListaPostagens"
-import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem"
+import ListaPostagens from "../../components/postagem/listapostagem/ListaPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
 
 function Home() {
     return (
         <>
             <div className="flex justify-center bg-gradient-to-r from-blue-500 to-green-500">
-                <div className="container grid grid-cols-2 text-white md:max-lg:grid-cols-1">
-                    <div className="flex flex-col items-center justify-center gap-4 py-4">
-                        <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
-                        <p className="text-xl">
-                            Expresse aqui seus pensamentos e
-                            opiniões
+                <div className="container grid grid-cols-1 md:grid-cols-2 text-white items-center px-4 py-8 gap-8">
+                    {/* Texto de boas-vindas */}
+                    <div className="flex flex-col items-center text-center md:text-left gap-4 ">
+                        <h2 className="text-4xl md:text-5xl font-bold font-dancing">Seja Bem Vinde!</h2>
+                        <p className="text-lg md:text-xl text-center">
+                            Expresse aqui seus pensamentos e opiniões
                         </p>
-                        <div className="flex justify-around gap-4">
-                            <div className="rounded border-white border-solid border-2 py-2 px-4 text-white hover:text-blue-950 hover:border-blue-950">
+                        <div className="flex justify-center md:justify-start">
+                            <div className="rounded border-white border-2 py-2 px-4 text-white hover:text-blue-950 hover:border-blue-950">
                                 <ModalPostagem />
                             </div>
                         </div>
                     </div>
-    
+
+                    {/* Imagem */}
                     <div className="flex justify-center">
                         <img
-                            className="w-2/3 md:max-lg:w-2/4"
-                            src="https://ik.imagekit.io/q5tv5x3k8/blog%20pessoal/blog-post-animate.svg?updatedAt=1739543770537"
+                            className="w-3/4 md:w-2/3 lg:w-1/2 max-w-[300px] md:max-w-none"
+                            src="https://ik.imagekit.io/q5tv5x3k8/blog%20pessoal/coding%20tech.svg?updatedAt=1740404198790"
                             alt="Imagem da Página Home"
                         />
                     </div>
@@ -30,7 +31,7 @@ function Home() {
             </div>
             <ListaPostagens />
         </>
-    )
+    );
 }
- 
-export default Home
+
+export default Home;
